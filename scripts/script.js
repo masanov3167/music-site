09466e312e00a329567e7c4831f242ee696e6e5c
Masanov <masanov3167@gmail.com>
2022-02-02 13:47:04 +0500

    add music

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,7 @@ new Vue({
           name: "Everybody Knows",
           artist: "Leonard Cohen",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          source: "../mp3/2.mp3",
           url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
           favorited: true
         },
@@ -29,7 +29,7 @@ new Vue({
           name: "Extreme Ways",
           artist: "Moby",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+          source: "../mp3/3.mp3",
           url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
           favorited: false
         },
@@ -45,7 +45,7 @@ new Vue({
           name: "The Final Victory",
           artist: "Haggard",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+          source: "../mp3/5.mp3",
           url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
           favorited: true
         },
@@ -53,7 +53,7 @@ new Vue({
           name: "Genius ft. Sia, Diplo, Labrinth",
           artist: "LSD",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+          source: "../mp3/6.mp3",
           url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
           favorited: false
         },
@@ -61,7 +61,7 @@ new Vue({
           name: "The Comeback Kid",
           artist: "Lindi Ortega",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+          source: "../mp3/7.mp3",
           url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
           favorited: true
         },
@@ -69,7 +69,7 @@ new Vue({
           name: "Overdose",
           artist: "Grandson",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+          source: "../mp3/8.mp3",
           url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
           favorited: false
         },
@@ -77,7 +77,7 @@ new Vue({
           name: "Rag'n'Bone Man",
           artist: "Human",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+          source: "../mp3/9.mp3",
           url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
           favorited: false
         }
